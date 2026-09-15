96a50c35b57042322bd8781d5ca5b602ddc8952e
Vakamalla Anitha <anithareddyvr.03@gmail.com>
2026-09-15 14:37:35 -0400

    python variables

---
+++ day1/01_variables.py
@@ -0,0 +1,36 @@
+#VARIABLES ARE CONTAINERS FOR STORING DATA VALUES
+#Variable naming rules
+#variable must start with(A-Z OR a-z)OR _(UNDERSCORE)
+#Variables contain alphabets, numbers, undescore
+#variables are case sensitive
+#Cannot start with a number
+#cannot use reserved keywords(if,for,while etc..)
+a=2
+b= "Anitha"
+print(a)
+print(b)
+
+
+
+a=2
+a="Anitha"
+a="Sunitha"
+print(a)
+
+
+
+#CASTING: CHANGING FROM ONE DATATYPE TO ANOTHER DATATYPE
+a=str(2)
+b=int(2)
+c=float(2)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+
+#CASE-SENSITIVE
+a=2
+A="Anitha"
+print(a)
