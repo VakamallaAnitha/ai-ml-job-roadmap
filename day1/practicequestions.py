33b69e9bb8b59bfdd60a71f6e0acc35b1a1118c9
Vakamalla Anitha <anithareddyvr.03@gmail.com>
2026-09-16 14:16:27 -0400

    practicequestions

---
+++ day1/practicequestions.py
@@ -0,0 +1,48 @@
+#DataTypes
+#Write python code to create a variable name with your name as a string and then print its data type
+a = "Anitha"
+print(a)
+print(type(a))
+
+
+#write code to create a variable with age as a integer and  print its data type
+a=24
+print(a)
+print(type(a))
+
+
+#Declare 2 variables a and b , assign them  values 10 and 5 respectively, and then swap their values without using a third variable
+a = 10
+b = 5
+a,b = b,a
+
+print(a)
+print(b)
+
+
+#Declare to 2 variables X and Y , their values 13,87 , print their sum
+x = 13
+y = 87
+print(x+y)
+
+
+#python expression that check if x is greater than y and prints "x is greater" if true, or "y is greater"
+x = 10
+y = 5
+if x>y:
+    print("x is greater")
+else:
+    print("y is greater")
+
+
+#print true if even else false
+a = 13
+if a%2 == 0:
+    print("true")
+else:
+    print("false")
+
+
+#calculate a number that counts square of number store in variable num  and prints output
+a = 3
+print(a**2)
