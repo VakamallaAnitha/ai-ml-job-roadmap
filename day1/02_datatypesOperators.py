b43a07cf5aa337c9076a61bb0e0e9cec30feaefd
Vakamalla Anitha <anithareddyvr.03@gmail.com>
2026-09-16 05:19:48 -0400

    datatypes,operators,python input

---
+++ day1/02_datatypesOperators.py
@@ -0,0 +1,39 @@
+#Variables can store data of different types, and different type can do different things
+#python datatypes
+#Numeric-Integrer,complex number, float
+#Dictonary
+#Boolean
+#set
+#sequence type-string,list,tuple
+
+
+
+#operators--Arithmetic,omparison,Assignment, logical,Bitwise 
+a=10
+b=20
+print("Addition", a+b)
+print("subtarction", a-b)
+print("division", a/b)
+print("modulo", a%b)
+print("multiplication", a*b)
+print("exponential", a**b)
+print("floor division", a//b)
+
+
+
+
+print("a==b", a==b)
+print("a!=b", a!=b)
+print("a>b", a>b)
+print("a<b", a<b)
+print("a>=b", a>=b)
+print("a<=b", a<=b)
+
+
+
+
+
+#input in python
+#input() function is used to take input from user
+a=int(input("enter your name:"))
+print(type(a))
